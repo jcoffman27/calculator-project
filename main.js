631e81dc5a018974d53cd9bb420f12543dda5b7f
jcoffman27 <73526442+jcoffman27@users.noreply.github.com>
2020-12-04 09:47:02 +0200

    Basic calculator functionality. no interface yet.

---
+++ main.js
@@ -0,0 +1,43 @@
+function add(num1, num2){
+    let newValue = num1 + num2;
+    console.log(newValue);
+    return newValue;
+};
+
+function subtract(num1, num2){
+    let newValue = num1 - num2;
+    console.log(newValue);
+    return newValue;
+};
+
+function multiply(num1, num2){
+    let newValue = num1 * num2;
+    console.log(newValue);
+    return newValue;
+};
+
+function divide(num1, num2){
+    let newValue = num1 / num2;
+    console.log(newValue);
+    return newValue;
+};
+
+function operate(){
+    let num1 = prompt("What is your first number: ");
+    let operation = prompt("Please enter an operation");
+    let num2 = prompt("What is your second number: ");
+
+    if (operation === "+"){
+        return add(num1, num2);
+    } else if (operation === "-") {
+        return subtract(num1, num2);
+    }else if (operation === "*") {
+        return multiply(num1, num2);
+    }else if (operation === "/") {
+        return divide(num1, num2);
+    } else {
+        return operate();
+    };
+};
+
+operate();
